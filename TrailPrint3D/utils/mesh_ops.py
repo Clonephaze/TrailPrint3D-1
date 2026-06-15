12b67a37ccb758328803974eeb15dafccc1698ff
EmGi <emgi3d@gmail.com>
2026-06-15 12:21:48 +0200

    removed replaced functions (get_building_data, create_element)

--- TrailPrint3D/utils/mesh_ops.py
+++ TrailPrint3D/utils/mesh_ops.py
@@ -1617,7 +1617,6 @@ def projection(operation, Mapobject, obj):
 
         single_color_mode_mesh_wireframe(obj, Mapobject)
 
-        raise Exception("Not implemented yet: singleColorMode_remesh")
 
     if operation == "singleColorMode_remesh":
 
@@ -1628,7 +1627,7 @@ def projection(operation, Mapobject, obj):
         thicker = single_color_mode_mesh_remesh(obj, Mapobject)
 
 
-        #remove_objects(thicker)
+        remove_objects(thicker)
 
     if operation == "negative":
         merge_with_map(Mapobject, obj, True)
