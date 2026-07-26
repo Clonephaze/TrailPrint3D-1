a1ea437e908dc023aac28e276877151b5536d81d
Jack Smith <jackdsmith2327@gmail.com>
2026-07-25 19:12:55 -0700

    import timezone, add timezone info to datetime calls

--- tests/test_geo_elevation.py
+++ tests/test_geo_elevation.py
@@ -11,7 +11,7 @@ import math
 import os
 import sys
 import traceback
-from datetime import datetime, timedelta
+from datetime import datetime, timezone
 
 # ---------------------------------------------------------------------------
 # Path setup
